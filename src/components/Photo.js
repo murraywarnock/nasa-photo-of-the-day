0a34f3c7e56a047b2930518a0bd48bb90851f1d2
MurrayWarnock <murray.warnock@gmail.com>
2021-05-12 13:09:55 -0600

    some design changes to Photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -4,22 +4,21 @@ import styled from 'styled-components';
 
 const StyledPhoto = styled.div`
 // element for entire component here
-  color: green;
+  color: black;
 //  within component listed by element
-  .blowup {
-    display: inline-block;
-  // h1 {
-    color: black;
+  .img-container {
+    display: block;
+    justify-content: center;
+    align-items: center;
+  }
+  .image {
+    /* height: 50%; */
+    max-width: 50%;
     &:hover {
-      color: red;
       transform: scale(1.2); //had to change display to inline-block for this to work
       transition: all .5s ease-in-out;
     }
     transition: all .5s ease-in-out;
-  }
-  .image {
-    /* height: 50%; */
-    width: 80%;
   }
   .img-text {
     width: 50%;
@@ -43,14 +42,17 @@ export default function Photo({imgDate, imgURL, title, imgExplanation}) {
       <h1><span className="blowup">NASA Image of the Day {imgDate}</span></h1>
       <div className="img-container" >
         {/* console.log(apod.url); */}
+        {/* <button>Previous</button> */}
         <img className = "image" src={imgURL} alt="NASA APOD"></img>
-        <p className = "imgTitle"> {title}</p>
-        <p className = "img-text">
-          {/* Read through the instructions in the README.md file to build your NASA
-          app! Have fun <span role="img" aria-label='go!'>🚀</span>! */}
-          {imgExplanation}
-        </p>
+        {/* <button>Next</button> */}
       </div>
+      <p className = "imgTitle "> {title}</p>
+      <p className = "img-text">
+
+        {/* Read through the instructions in the README.md file to build your NASA
+        app! Have fun <span role="img" aria-label='go!'>🚀</span>! */}
+        {imgExplanation}
+        </p>
     </div>
     </StyledPhoto>
     )
